2b8040a45198fd7d2f6441ac429b1e778426e912
Jaybee van der Linde <nubllett@hotmail.com>
2024-05-25 17:43:17 +0200

    Add project files.

---
+++ CSV Editor/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CSV_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.Grid1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1040, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuSave,
+            this.mnuSaveAs,
+            this.toolStripMenuItem1,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(180, 22);
+            this.mnuOpen.Text = "&Open...";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(180, 22);
+            this.mnuSave.Text = "&Save";
+            // 
+            // mnuSaveAs
+            // 
+            this.mnuSaveAs.Name = "mnuSaveAs";
+            this.mnuSaveAs.Size = new System.Drawing.Size(180, 22);
+            this.mnuSaveAs.Text = "Save &As...";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(180, 22);
+            this.mnuExit.Text = "E&xit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // Grid1
+            // 
+            this.Grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid1.Location = new System.Drawing.Point(34, 54);
+            this.Grid1.Name = "Grid1";
+            this.Grid1.Size = new System.Drawing.Size(982, 389);
+            this.Grid1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 493);
+            this.Controls.Add(this.Grid1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuSaveAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.DataGridView Grid1;
+    }
+}
+
---
+++ CSV Editor/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace CSV_Editor
+{
+    public partial class Form1 : Form
+    {
+        bool saved = true;
+
+        string csvFile = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!saved)
+            {
+                if (MessageBox.Show("You have unsaved data!\nAre you sure you want to quit?", "Unsaved data!",
+                     MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) != DialogResult.Yes)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "CSV files *.csv|*.csv";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                csvFile = dlg.FileName;
+                saved = false;
+                LoadFile();
+            }
+        }
+
+        private void LoadFile()
+        {
+            string rawData = LoadData(csvFile);
+
+            string[] cols = getColumns(rawData);
+
+            DataTable table=new DataTable();
+            DataSet ds = new DataSet();
+            
+            foreach(string col in cols)
+            {
+                table.Columns.Add(col);
+            }
+
+            string data = stripData(rawData);
+            string[] lines = null;
+            if (data.Contains("\r\n"))
+            {
+                lines = data.Split(new char[] { '\r', '\n' });
+            }
+            else
+            {
+                lines = data.Split('\n');
+            }
+
+            foreach(string line in lines)
+            {
+                DataRow row = table.NewRow();
+                string[] colValues = line.Split(',');
+                for(int i = 0; i<row.Table.Columns.Count - 1; i++)
+                {
+                    row[i] = colValues[i];
+                }
+            }
+
+            ds.Tables.Add(table);
+            Grid1.DataSource = ds;
+        }
+
+        private string LoadData(string file)
+        {
+            string retVal = "";
+
+            System.IO.FileStream fs = new System.IO.FileStream(file,System.IO.FileMode.Open,System.IO.FileAccess.Read);
+            byte[] buffer = new byte[fs.Length];
+
+            fs.Read(buffer, 0, buffer.Length);
+            fs.Close();
+
+            retVal = Encoding.UTF8.GetString(buffer);
+
+            Array.Clear(buffer,0, buffer.Length);
+
+            return retVal;
+        }
+
+        private string[] getColumns(string data)
+        {
+            string[] cols = null;
+
+            //we only need the first line.
+            string line = "";
+            if (data.Contains("\r\n"))
+            {
+                line = data.Split(new char[] { '\r', '\n' })[0];
+            }
+            else if(data.Contains("\n"))
+            {
+                line = data.Split('n')[0];
+            }
+
+            cols = line.Split(',');
+
+            return cols;
+        }
+
+        private string stripData(string rawData)
+        {
+            string[] data = null;
+
+
+            if(rawData.Contains("\r\n"))
+            {
+                data=rawData.Split(new char[] { '\r', '\n' });
+            }
+            else
+            {
+                data = rawData.Split('\n');
+            }
+            //we don't want the first line, this contains the heading names
+            string cleanData = "";
+            for(int i =1; i < data.Length;)
+            {
+                //we don't want an empty line for data.
+                if (!string.IsNullOrEmpty( data[i]))
+                    cleanData += data[i];   
+            }
+
+
+            return cleanData;
+        }
+    }
+}
